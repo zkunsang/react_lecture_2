c0cfaf72d8587c3b7abd00b17f1edd47fc98777e
zkunsang <zkunsang@gmail.com>
2021-12-29 04:30:35 +0900

    redux

--- cra-test/src/App.js
+++ cra-test/src/App.js
@@ -1,15 +1,54 @@
-import React, { useState } from "react";
-
-import { getNextFriend } from "./data";
-import FriendPage from "./container/FriendPage";
+import React, { useContext, createContext, useReducer } from "react";
+import { createStore } from "redux";
+import { Provider, useSelector, useDispatch } from "react-redux";
 
 export default function App() {
   return (
+    <Provider store={store}>
+      <User />
+      <Product />
+    </Provider>
+  );
+}
+
+const INITIAL_STATE = {
+  user: { name: "mike" },
+  product: { name: "iphone" },
+};
+
+function reducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case "setUserName":
+      return {
+        ...state,
+        user: { ...state.user, name: action.name },
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+function User() {
+  console.log("User render");
+
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  return (
     <div>
-      <FriendPage />
+      <p>{`${user.name}님 안녕하세요`}</p>
+      <button onClick={() => dispatch({ type: "setUserName", name: "john" })}>
+        사용자 이름 수정
+      </button>
     </div>
   );
 }
 
-const MAX_AGE_LIMIT = 100;
-const AGE_LIMIT_OPTIONS = [15, 20, 25, MAX_AGE_LIMIT];
+function Product() {
+  console.log("product render");
+
+  const product = useSelector((state) => state.product);
+  return <p>{`제품 이름: ${product.name}`}</p>;
+}
